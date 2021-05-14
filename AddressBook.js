93b758fdce6e0d1c66b62970331fd7ff8e94586d
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-05-15 01:38:55 +0530

    ADD: method to find person by name and edit details

--- AddressBook.js
+++ AddressBook.js
@@ -14,20 +14,22 @@ class Person {
   //constructor(regex added to each element)
   constructor(...params) {
 
-    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    //first name
+    let nameRegex = RegExp("^[A-Z]{1}[a-z0-9]{2,}$");
     if (nameRegex.test(params[0])) {
       this.firstName = params[0];
     } else {
       throw "Invalid first name!";
     }
 
-    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    //last name
     if (nameRegex.test(params[1])) {
       this.lastName = params[1];
     } else {
       throw "Invalid last name!";
     }
 
+    //address
     let addressRegex = RegExp("[A-Za-z0-9./-]{3,}$");
     if (addressRegex.test(params[2])) {
       this.address = params[2];
@@ -35,6 +37,7 @@ class Person {
       throw "Invalid address!";
     }
 
+    //city
     let cityRegex = RegExp("[A-Za-z0-9./]{3,}$");
     if (cityRegex.test(params[3])) {
       this.city = params[3];
@@ -42,6 +45,7 @@ class Person {
       throw "Invalid city!";
     }
 
+    //state
     let stateRegex = RegExp("[A-Za-z0-9./]{3,}$");
     if (stateRegex.test(params[4])) {
       this.state = params[4];
@@ -49,6 +53,7 @@ class Person {
       throw "Invalid state!";
     }
     
+    //zip
     let zipRegex = RegExp("^[1-9]{1}[0-9]{2,6}$");
     if (zipRegex.test(params[5])) {
       this.zip = params[5];
@@ -56,13 +61,15 @@ class Person {
       throw "Invalid zip!";
     }
     
-    let phoneRegex = RegExp("^[+][0-9]{1,2}[0-9]{3,13}$");
+    //phone number
+    let phoneRegex = RegExp("^[+][0-9]{1,2}\\s[0-9]{3,13}$");
     if (phoneRegex.test(params[6])) {
       this.phoneNumber = params[6];
     } else {
       throw "Invalid phone number!";
     }
     
+    //email
     let emailRegex = RegExp(
       "^^(?!\\.)[A-Za-z0-9]+([._%+-]?[0-9])*@[A-Za-z0-9-]+\\.[a-zA-Z]{2,}(\\.[A-Za-z]{2,})?$"
     );
@@ -73,93 +80,25 @@ class Person {
     }
   }
 
-  //to set firstname with conditions
-  setFirstName(newFirstName) {
-    let fNameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
-    if (fNameRegex.test(newFirstName)) {
-      this.firstName = newFirstName;
-    } else {
-      throw "Invalid first name!";
-    }
-  }
-
-  //to update lastname with conditions
-  setLastName(newLastName) {
-    let lNameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
-    if (lNameRegex.test(newFirstName)) {
-      this.lastName = newLastName;
-    } else {
-      throw "Invalid last name!";
-    }
-  }
-
-  //to update address with conditions
-  setAddress(newAddress) {
-    let addressRegex = RegExp("[A-Za-z0-9./]{3,}$");
-    if (addressRegex.test(newAddress)) {
-      this.address = newAddress;
-    } else {
-      throw "Invalid address!";
-    }
-  }
-
-  //to update city with conditions
-  setCity(newCity) {
-    let cityRegex = RegExp("[A-Za-z0-9./]{3,}$");
-    if (cityRegex.test(newCity)) {
-      this.city = newCity;
-    } else {
-      throw "Invalid city!";
-    }
-  }
-
-  //to update state with conditions
-  setState(newState) {
-    let stateRegex = RegExp("[A-Za-z0-9./]{3,}$");
-    if (stateRegex.test(newState)) {
-      this.state = newState;
-    } else {
-      throw "Invalid state!";
-    }
-  }
-
-  //to update zip with conditions
-  setZip(newZip) {
-    let zipRegex = RegExp("^[1-9]{1}[0-9]{2,6}$");
-    if (zipRegex.test(newZip)) {
-      this.zip = newZip;
-    } else {
-      throw "Invalid zip!";
-    }
-  }
-
-  //to update phonenumber with conditions
-  setPhoneNumber(newPhoneNumber) {
-    let phoneRegex = RegExp("^[+][0-9]{1,2}[0-9]{3,13}$");
-    if (phoneRegex.test(newPhoneNumber)) {
-      this.phoneNumber = newPhoneNumber;
-    } else {
-      throw "Invalid phone number!";
-    }
-  }
-
-  //to update email with conditions
-  setEmail(newEmail) {
-    let emailRegex = RegExp(
-      "^^(?!\\.)[A-Za-z0-9]+([._%+-]?[0-9])*@[A-Za-z0-9-]+\\.[a-zA-Z]{2,}(\\.[A-Za-z]{2,})?$"
-    );
-    if (emailRegex.test(newEmail)) {
-      this.email = newEmail;
-    } else {
-      throw "Invalid email!";
-    }
+  //toString method to get all the details
+  toString(){
+    return "[\n"+
+    ' first name = '+ this.firstName+
+    '\n last name = '+this.lastName+
+    '\n address = '+this.address+
+    '\n city = '+this.city+
+    '\n state = '+this.state+
+    '\n zip = '+this.zip+
+    '\n phone number = '+this.phoneNumber+
+    '\n email = '+this.email+
+    "\n]";
   }
 }
 
 //object creation(adding new person)
 let person1 = new Person(
   "Kiran",
-  "kumar",
+  "Kumar",
   "address",
   "city",
   "state",
@@ -171,7 +110,7 @@ let person1 = new Person(
 //object creation(adding new person)
 let person2 = new Person(
   "Kiran2",
-  "kumar2",
+  "Kumar2",
   "address2",
   "city2",
   "state2",
@@ -180,9 +119,27 @@ let person2 = new Person(
   "email2@gmail.com"
 );
 
+/**
+ * UC3: adding to array
+ */
 //array to store object of person
 let addressBookArray = new Array();
 
 //adding persons to the addressbook array
 addressBookArray.push(person1);
 addressBookArray.push(person2);
+
+/**
+ * UC4: finding person by name and edit details
+ */
+//finding person by name
+function isNameMatches(person){
+  return (person.firstName === 'Kiran' || person.lastName === 'Kiran');
+}
+
+let personWanted = addressBookArray.find(isNameMatches);
+console.log("person found!: \n"+personWanted);
+
+//editing details
+personWanted.email = 'newpersonsemai@gmail.com';
+console.log("after changing email: \n"+personWanted);
