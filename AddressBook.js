63bcbc85e78257356d6466dc0194d7914c913009
kirankumae-Matham96 <mathamkirankumar96@gamil.com>
2021-05-15 00:52:06 +0530

    MODIFIED: Every element is verified with regex pattern

--- AddressBook.js
+++ AddressBook.js
@@ -11,60 +11,152 @@ class Person {
   phoneNumber;
   email;
 
-  //constructor
+  //constructor(regex added to each element)
   constructor(...params) {
-    this.firstName = params[0];
-    this.lastName = params[1];
-    this.address = params[2];
-    this.city = params[3];
-    this.state = params[4];
-    this.zip = params[5];
-    this.phoneNumber = params[6];
-    this.email = params[7];
+
+    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    if (nameRegex.test(params[0])) {
+      this.firstName = params[0];
+    } else {
+      throw "Invalid first name!";
+    }
+
+    let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    if (nameRegex.test(params[1])) {
+      this.lastName = params[1];
+    } else {
+      throw "Invalid last name!";
+    }
+
+    let addressRegex = RegExp("[A-Za-z0-9./-]{3,}$");
+    if (addressRegex.test(params[2])) {
+      this.address = params[2];
+    } else {
+      throw "Invalid address!";
+    }
+
+    let cityRegex = RegExp("[A-Za-z0-9./]{3,}$");
+    if (cityRegex.test(params[3])) {
+      this.city = params[3];
+    } else {
+      throw "Invalid city!";
+    }
+
+    let stateRegex = RegExp("[A-Za-z0-9./]{3,}$");
+    if (stateRegex.test(params[4])) {
+      this.state = params[4];
+    } else {
+      throw "Invalid state!";
+    }
+    
+    let zipRegex = RegExp("^[1-9]{1}[0-9]{2,6}$");
+    if (zipRegex.test(params[5])) {
+      this.zip = params[5];
+    } else {
+      throw "Invalid zip!";
+    }
+    
+    let phoneRegex = RegExp("^[+][0-9]{1,2}[0-9]{3,13}$");
+    if (phoneRegex.test(params[6])) {
+      this.phoneNumber = params[6];
+    } else {
+      throw "Invalid phone number!";
+    }
+    
+    let emailRegex = RegExp(
+      "^^(?!\\.)[A-Za-z0-9]+([._%+-]?[0-9])*@[A-Za-z0-9-]+\\.[a-zA-Z]{2,}(\\.[A-Za-z]{2,})?$"
+    );
+    if (emailRegex.test(params[7])) {
+      this.email = params[7];
+    } else {
+      throw "Invalid email!";
+    }
   }
 
-  //to update firstname
-  updateFirstName(newFirstName) {
-    this.firstName = newFirstName;
+  //to set firstname with conditions
+  setFirstName(newFirstName) {
+    let fNameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    if (fNameRegex.test(newFirstName)) {
+      this.firstName = newFirstName;
+    } else {
+      throw "Invalid first name!";
+    }
   }
 
-  //to update lastname
-  updateLastName(newLastName) {
-    this.lastName = newLastName;
+  //to update lastname with conditions
+  setLastName(newLastName) {
+    let lNameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+    if (lNameRegex.test(newFirstName)) {
+      this.lastName = newLastName;
+    } else {
+      throw "Invalid last name!";
+    }
   }
 
-  //to update address
-  updateAddress(newAddress) {
-    this.address = newAddress;
+  //to update address with conditions
+  setAddress(newAddress) {
+    let addressRegex = RegExp("[A-Za-z0-9./]{3,}$");
+    if (addressRegex.test(newAddress)) {
+      this.address = newAddress;
+    } else {
+      throw "Invalid address!";
+    }
   }
 
-  //to update city
-  updateCity(newCity) {
-    this.city = newCity;
+  //to update city with conditions
+  setCity(newCity) {
+    let cityRegex = RegExp("[A-Za-z0-9./]{3,}$");
+    if (cityRegex.test(newCity)) {
+      this.city = newCity;
+    } else {
+      throw "Invalid city!";
+    }
   }
 
-  //to update state
-  updateState(newState) {
-    this.state = newState;
+  //to update state with conditions
+  setState(newState) {
+    let stateRegex = RegExp("[A-Za-z0-9./]{3,}$");
+    if (stateRegex.test(newState)) {
+      this.state = newState;
+    } else {
+      throw "Invalid state!";
+    }
   }
 
-  //to update zip
-  updateZip(newZip) {
-    this.zip = newZip;
+  //to update zip with conditions
+  setZip(newZip) {
+    let zipRegex = RegExp("^[1-9]{1}[0-9]{2,6}$");
+    if (zipRegex.test(newZip)) {
+      this.zip = newZip;
+    } else {
+      throw "Invalid zip!";
+    }
   }
 
-  //to update phonenumber
-  updatePhoneNumber(newPhoneNumber) {
-    this.phoneNumber = newPhoneNumber;
+  //to update phonenumber with conditions
+  setPhoneNumber(newPhoneNumber) {
+    let phoneRegex = RegExp("^[+][0-9]{1,2}[0-9]{3,13}$");
+    if (phoneRegex.test(newPhoneNumber)) {
+      this.phoneNumber = newPhoneNumber;
+    } else {
+      throw "Invalid phone number!";
+    }
   }
 
-  //to update email
-  updateEmail(newEmail) {
-    this.email = newEmail;
+  //to update email with conditions
+  setEmail(newEmail) {
+    let emailRegex = RegExp(
+      "^^(?!\\.)[A-Za-z0-9]+([._%+-]?[0-9])*@[A-Za-z0-9-]+\\.[a-zA-Z]{2,}(\\.[A-Za-z]{2,})?$"
+    );
+    if (emailRegex.test(newEmail)) {
+      this.email = newEmail;
+    } else {
+      throw "Invalid email!";
+    }
   }
 }
 
-//object creation
+//object creation(adding new person)
 let person1 = new Person(
   "Kiran",
   "kumar",
@@ -76,5 +168,4 @@ let person1 = new Person(
   "email@gmail.com"
 );
 
-//printing all the details
 console.log(person1);
